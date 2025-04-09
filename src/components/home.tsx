b9d9f46880af37c1c8aadfe246641c07d0f6003b
Sarthak Chaturvedi <alphabyte.founders@gmail.com>
2025-04-09 22:56:55 +0530

    Update home.tsx

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -355,8 +355,7 @@ const Home = () => {
           <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="mb-4 md:mb-0">
               <p className="text-sm text-gray-600">
-                © {new Date().getFullYear()} Philips Invoice & PO Verification
-                System
+                © {new Date().getFullYear()} Powered by AlphaByte
               </p>
             </div>
             <div className="flex space-x-4">

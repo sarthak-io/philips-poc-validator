7087f3f8b7a52389bedd0a35fc25e52e00091069
sarthak-io <alphabyte.founders@gmail.com>
2025-04-09 17:31:26 +0000

    final commit

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -173,7 +173,7 @@ const Home = () => {
               />
             </svg>
             <h1 className="text-xl font-bold">
-              Philips Invoice & PO Verification System
+              AlphaByte Invoice & PO Verification System
             </h1>
           </div>
           <div>
@@ -355,8 +355,8 @@ const Home = () => {
           <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="mb-4 md:mb-0">
               <p className="text-sm text-gray-600">
-                © {new Date().getFullYear()} Philips Invoice & PO Verification
-                System
+                © {new Date().getFullYear()} AlphaByte Invoice & PO
+                Verification System
               </p>
             </div>
             <div className="flex space-x-4">
